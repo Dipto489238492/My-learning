6b4a830c3b2d7d6659ceb624f2a9015369f51289
Dipto489238492 <dipunc887@gmail.com>
2026-08-15 12:04:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,5 +12,5 @@ function doMath() {
         let lower3SD = mean - (3 * sd);
         let upper3SD = mean + (3 *sd);
         // 5. Inject the final answar
-        document.getElementById("answerArea").innerHTML = "2SD range :" + lower2SD + " to " + upper2SD + "<br> 3SD range :" + lower3SD + " to " + upper3SD;
+        document.getElementById("answerArea").innerHTML = "2SD range :" + lower2SD.toFixed(2) + " to " + upper2SD.toFixed(2) + "<br> 3SD range :" + lower3SD.toFixed(2) + " to " + upper3SD.toFixed(2);
       }
